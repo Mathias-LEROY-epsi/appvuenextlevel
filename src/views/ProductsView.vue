63e690c89c5cf5dc21cf6c37c6b7b061777a10d9
Lucile16 <lulubeucher@gmail.com>
2023-01-05 15:16:29 +0100

    feat/startingproductlist

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Products from "../components/Products.vue";
+</script>
+
+<template>
+  <h1>Liste des produits</h1>
+  <main>
+    <Products />
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
