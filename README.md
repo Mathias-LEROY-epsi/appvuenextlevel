ccddc7bd505ea8d6c8c237b4a99f4d8d9c18653e
Mathias LEROY <mathias.leroy@epsi.fr>
2023-01-08 22:02:23 +0100

    feat/refonte de la page loginView + persistance des données dans le sessionStorage

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # projet-vue-sfc
+
 Ce projet est construit en Vue 3 avec Vite.
 
 ## Fonctionnalités
+
 Ce projet utilise l'API [dummyJSON](https://dummyjson.com/docs) pour afficher des utilisateurs, articles, et un panier. L'api possède aussi une connexion.
 
 # Connexion / Login
-Pour se connecter, il faut utiliser le mot de passe suivant : **aUTdmmmbH**
+
+Pour se connecter, il faut utiliser le mot de passe suivant :
+
+- Utilisateur n°1: **9uQFF1Lh**
+- Utilisateur n°5: **aUTdmmmbH**
+- Utilisateur n°10: **i0xzpX**
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,12 +8,55 @@
           <p v-if="passwordIsWrong" style="color: red">
             Mot de passe incorrect
           </p>
-          <button @click="findUser()" type="submit">Se connecter</button>
+          <button @click="logIn()" type="submit" class="logIn">
+            Se connecter
+          </button>
         </form>
       </div>
     </div>
     <div v-else>
-      <h1 style="color: greenyellow">Vous êtes connecté</h1>
+      <div v-for="user in data" class="results">
+        <div class="userData">
+          <ul style="display: flex; flex-direction: column">
+            <li>
+              <img :src="user.image" alt="r.lastName" style="width: 300px" />
+            </li>
+            <li>
+              <h2>
+                Vous êtes connecté {{ user.lastName }} {{ user.firstName }}
+              </h2>
+            </li>
+            <button
+              @click="logOut()"
+              type="submit"
+              class="logOut"
+              style="width: 200px; margin-inline: auto; font-size: medium"
+            >
+              Se déconnecter
+            </button>
+          </ul>
+          <ul>
+            <li>
+              <strong>Date de naissance:</strong> {{ user.birthDate }} ({{
+                user.age
+              }}
+              ans)
+            </li>
+            <li><strong>Sexe:</strong> {{ user.gender }}</li>
+            <li style="overflow-wrap: break-word">
+              <strong>Mail:</strong> {{ user.email }}
+            </li>
+            <li><strong>Tel:</strong> {{ user.phone }}</li>
+            <li><strong>Adresse:</strong> {{ user.address.address }}</li>
+            <li><strong>Code postal:</strong> {{ user.address.postalCode }}</li>
+            <li>
+              <strong>Ville:</strong> {{ user.address.city }} ({{
+                user.address.state
+              }})
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,33 +70,43 @@ export default {
     return {
       isConnected: false,
       passwordIsWrong: false,
-      data: null,
+      data: [],
       password: "",
     };
   },
 
   created: async function () {
-    if (localStorage.getItem("isConnected") === "true") {
+    if (sessionStorage.getItem("isConnected") === "true") {
       this.isConnected = true;
+      this.data = JSON.parse(sessionStorage.getItem("data"));
     }
   },
 
   methods: {
-    findUser: async function () {
+    logIn: async function () {
       const pwd = this.password;
 
       await axios
         .get(`https://dummyjson.com/users/filter?key=password&value=${pwd}`)
         .then((res) => {
           this.data = res.data.users;
-          console.log(this.isConnected);
+          console.log(this.data);
           if (this.data.length === 0) {
             this.passwordIsWrong = !this.passwordIsWrong;
           } else {
             this.isConnected = !this.isConnected;
-            window.localStorage.setItem("isConnected", this.isConnected);
+            sessionStorage.setItem("isConnected", this.isConnected);
+            sessionStorage.setItem("data", JSON.stringify(this.data));
           }
         });
+    },
+    logOut: async function () {
+      this.isConnected = !this.isConnected;
+      sessionStorage.setItem("isConnected", this.isConnected);
+      sessionStorage.setItem("data", JSON.stringify((this.data = [])));
+    },
+    getData: async function () {
+      console.log(this.data);
     },
   },
 };
@@ -83,18 +136,59 @@ label {
 
 input {
   padding: 0.5rem;
-  border: 1px solid #ccc;
+  border: 2px solid var(--vt-c-black-soft);
   border-radius: 4px;
   margin-bottom: 1rem;
   width: 100%;
 }
 
-button[type="submit"] {
+.userData {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 2rem;
+  width: 80vw;
+}
+
+.logIn,
+.logOut {
+  background-color: var(--vt-c-black-soft);
   padding: 0.5rem 1rem;
-  background-color: var(--vt-c-green);
   color: #fff;
   border: none;
   border-radius: 4px;
   cursor: pointer;
+}
+
+.logIn:hover {
+  background-color: var(--vt-c-green);
+}
+
+.logOut:hover {
+  background-color: var(--vt-c-red);
+}
+
+.results ul {
+  list-style: none;
+  text-align: left;
+  border: var(--color-primary) 5px solid;
+  border-radius: var(--border-radius);
+}
+
+.results ul li {
+  padding: 8px;
+  letter-spacing: 2px;
+}
+
+.results ul li a {
+  color: var(--color-primary-light);
+}
+
+.results ul li strong {
+  color: var(--vt-c-red);
+  text-decoration: underline;
+  text-underline-offset: 4px;
+  font-size: larger;
 }
 </style>
